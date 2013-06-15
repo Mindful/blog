536c7eaf747e62a99bf2a7da6b488ebafb5b6a70
Mindful <jtanner2@pacbell.net>
2013-06-14 19:29:10 -0700

    better post indexing

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130615010730) do
+ActiveRecord::Schema.define(version: 20130615021447) do
 
   create_table "posts", force: true do |t|
     t.string   "content_markdown"
     t.string   "content_html"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "title"
   end
 
   create_table "users", force: true do |t|
