760cc125a86e2a6f91582cb923db4af03c962cb0
Mindful <jtanner2@pacbell.net>
2013-06-15 19:39:28 -0700

    working endless scrolling

--- app/assets/javascripts/posts.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-  return $('a.load-more-posts').on('inview', function(e, visible) { 
-    if (!visible) { //the if(!visible) is because 'inview' fires when the element comes into AND GOES OUT OF view
-      return;
-    }
-    return $.getScript($(this).attr('href'));
-  });
-});
