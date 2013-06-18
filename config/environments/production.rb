a8929304d33c5477aa0f747c09a8e7a95900b1a3
Mindful <jtanner2@pacbell.net>
2013-06-18 13:36:59 -0700

    demo/fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Blog::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false #enabled, although likely only necessary for testing purposes
+  config.serve_static_assets = true #enabled, and necessary for Heroku. Might not be for a different host.
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
