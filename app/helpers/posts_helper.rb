f27376b211e54ee7497e3c3df2d874837792373a
Joshua Tanner <jtanner2@pacbell.net>
2013-07-14 20:04:44 -0700

    fix pagination like beast

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,10 @@
 module PostsHelper
+
+	def post_next_page_path(collection)
+		url_for(params.merge(:page => collection.next_page))
+	end
+
+	def post_previous_page_path(collection)
+		url_for(params.merge(:page => collection.previous_page))
+	end
 end
