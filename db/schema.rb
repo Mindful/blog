bcc0b937a8201f882974f6cf2fd9fecb5fb2ce57
colin <colinarobinson@gmail.com>
2013-07-17 17:02:56 -0400

    Styling added for tags, login page, and sidebar, as well as misc small changes

--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 20130714223219) do
     t.string   "url"
   end
 
-  add_index "posts", ["url"], name: "index_posts_on_url", unique: true
+  add_index "posts", ["url"], name: "index_posts_on_url", unique: true, using: :btree
 
   create_table "taggings", force: true do |t|
     t.integer  "tag_id"
@@ -44,8 +44,8 @@ ActiveRecord::Schema.define(version: 20130714223219) do
     t.datetime "created_at"
   end
 
-  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id"
-  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context"
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id", using: :btree
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context", using: :btree
 
   create_table "tags", force: true do |t|
     t.string "name"
@@ -60,7 +60,7 @@ ActiveRecord::Schema.define(version: 20130714223219) do
     t.string   "remember_token"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
 
 end
