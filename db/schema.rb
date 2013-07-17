cb04b351fdc7796375fb2f361ac56d6c62dd34a5
colin <colinarobinson@gmail.com>
2013-07-17 13:49:22 -0400

    preparing to merge into search branch

--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20130620180450) do
     t.string   "url"
   end
 
-  add_index "posts", ["url"], name: "index_posts_on_url", unique: true, using: :btree
+  add_index "posts", ["url"], name: "index_posts_on_url", unique: true
 
   create_table "taggings", force: true do |t|
     t.integer  "tag_id"
@@ -34,8 +34,8 @@ ActiveRecord::Schema.define(version: 20130620180450) do
     t.datetime "created_at"
   end
 
-  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id", using: :btree
-  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context", using: :btree
+  add_index "taggings", ["tag_id"], name: "index_taggings_on_tag_id"
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context"
 
   create_table "tags", force: true do |t|
     t.string "name"
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 20130620180450) do
     t.string   "remember_token"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
