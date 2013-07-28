ef1ee581ecde2d5417624656f474fd1e939ae632
Joshua Tanner <jtanner2@pacbell.net>
2013-07-27 17:15:00 -0700

    javascript wip

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -6,12 +6,12 @@ class SubscriptionsController < ApplicationController
 		#when we eventually save, rescue to catch exceptions
 		if (@subscription.valid?) 
 			#render :js => "alert('you were successfuly subscribed #{email}');"
-			subscribedWindow "Thank you for subscribing! A confirmation email has been sent to #{email}."
+			subscribed_window "Thank you for subscribing! A confirmation email has been sent to #{email}."
 			#Use JS to make something pretty and clear out the subscribe box of text
 			#Also, confirmation email
 		else
 			#detect error type. if empty, do nothing. if invalid, say invalid. if duplicate or exception, say it
-			subscribedWindow "could not subscribe you. already subscribed?"
+			subscribed_window "could not subscribe you. already subscribed?"
 		end
 	end
 
@@ -22,7 +22,7 @@ class SubscriptionsController < ApplicationController
 	end
 
 	private
-	  def subscribedWindow(text)
+	  def subscribed_window(text)
 	  	render :js => "openSubscribedWindow('#{text}');"
 	  end
 
