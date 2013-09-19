203d68de9822f59aa27452362d21dc2dd990a464
Joshua Tanner <jtanner2@pacbell.net>
2013-09-18 18:08:58 -0700

    super basic gallery

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,6 +1,6 @@
 class Picture < ActiveRecord::Base
 
-  has_attached_file :image, :styles => { :large => "700x700>", :medium => "350x350>", :thumb => "150x150>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => { :large => "700x700>", :medium => "400x400>", :thumb => "200x200>" }, :default_url => "/images/:style/missing.png"
   
   validates :name, presence:   true,
                     uniqueness: { case_sensitive: false }
@@ -8,4 +8,12 @@ class Picture < ActiveRecord::Base
   validates_attachment :image, :presence => true,
   :content_type => { :content_type => /^image\/(png|gif|jpeg|jpg|bmp)/ },
   :size => { :in => 0..100.megabytes }
+
+  def url_large
+  	self.image.url(:large, timestamp: false)
+  end
+
+  def url_medium
+  	self.image.url(:medium, timestamp: false)
+  end
 end
