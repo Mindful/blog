3a071a345e3a0bf431e2e2034952a3a80f975917
Joshua Tanner <jtanner2@pacbell.net>
2013-09-24 22:02:09 -0700

    locations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130918220246) do
+ActiveRecord::Schema.define(version: 20130925045811) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -38,6 +38,14 @@ ActiveRecord::Schema.define(version: 20130918220246) do
   end
 
   add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
+
+  create_table "locations", force: true do |t|
+    t.string   "name"
+    t.decimal  "latitude",   precision: 10, scale: 6
+    t.decimal  "longitude",  precision: 11, scale: 6
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "pictures", force: true do |t|
     t.string   "name"
