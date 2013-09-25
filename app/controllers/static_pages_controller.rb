208a5ede3c5ebc825033a00e82ddfb0a7871d7a0
Joshua Tanner <jtanner2@pacbell.net>
2013-09-25 14:51:53 -0700

    mapwork

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,6 +6,7 @@ class StaticPagesController < ApplicationController
   end
 
   def map
+  	@locations = Location.all
   end
 
   def jet
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,11 @@
 class Location < ActiveRecord::Base
 	belongs_to :post
 
+
+	#Image must be 'dot' or 'pushpin'
+
+
+	def icon
+		image+'.png'
+	end
 end
